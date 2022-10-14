657d94ca9a8facb7bb7b1099587e1c062eec74c2
aljocadi <aljocadi@gmail.com>
2022-10-14 09:32:28 +0200

    Con gestion de tibreak 1

--- MarcadorWindows/MainWindow.xaml.cs
+++ MarcadorWindows/MainWindow.xaml.cs
@@ -20,9 +20,89 @@ namespace MarcadorWindows
     /// </summary>
     public partial class MainWindow : Window
     {
+        Marcador miMarcador=new Marcador("Jose","Pepe",3);
+        private bool estadoEdicion;
         public MainWindow()
         {
             InitializeComponent();
+            estadoEdicion=false;
+            miMarcador.PartidoFinalizado += OnPartidoFinalizado;
+        }
+
+        /// <summary>
+        /// Método encargado de Actualizar el marcador.
+        /// </summary>
+        private void ActualizaMarcador()
+        {
+            TextPointsPlayer1.Text = miMarcador.PuntosLocal;
+            TextPointsPlayer2.Text = miMarcador.PuntosVisitante;
+            TextSet1Player1.Text = miMarcador.MarcadorLocal[0].ToString();
+            TextSet2Player1.Text = miMarcador.MarcadorLocal[1].ToString();
+            TextSet3Player1.Text = miMarcador.MarcadorLocal[2].ToString();
+            TextSet1Player2.Text = miMarcador.MarcadorVisitante[0].ToString();
+            TextSet2Player2.Text = miMarcador.MarcadorVisitante[1].ToString();
+            TextSet3Player2.Text = miMarcador.MarcadorVisitante[2].ToString();
+        }
+
+        /// <summary>
+        /// Delegado encargado de dar un punto local
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            miMarcador.PuntoLocal();
+            ActualizaMarcador();
+        }
+
+
+        /// <summary>
+        /// Delegado encargado de dar un punto visitante
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            miMarcador.PuntoVisitante();
+            ActualizaMarcador();
+        }
+        
+        /// <summary>
+        /// Delegado encargado de poner los campos de texto en modo edición
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            estadoEdicion = !estadoEdicion;
+            if (estadoEdicion)
+            {
+                BtnEdit.Content = "Edición on";
+                BtnEdit.Background = new SolidColorBrush(Colors.DarkGreen);
+
+            }
+            else
+            {
+                BtnEdit.Content = "Edición off";
+                BtnEdit.Background = new SolidColorBrush(Colors.DarkRed);
+            }
+            TextSet1Player1.IsEnabled = estadoEdicion;
+            TextSet2Player1.IsEnabled = estadoEdicion;
+            TextSet3Player1.IsEnabled = estadoEdicion;
+            TextSet1Player2.IsEnabled = estadoEdicion;
+            TextSet2Player2.IsEnabled = estadoEdicion;
+            TextSet3Player2.IsEnabled = estadoEdicion;
+            TextPointsPlayer1.IsEnabled = estadoEdicion;
+            TextPointsPlayer2.IsEnabled = estadoEdicion;
+
+        }
+
+        private void OnPartidoFinalizado(object fuente, PartidoEventArgs e)
+        {
+            BtnEdit.IsEnabled = false;
+            BtnPoinsPlayer1.IsEnabled = false;
+            BtnPoinsPlayer2.IsEnabled = false;
+            MessageBox.Show("El ganador del partido es: "+ e.Ganador);
         }
     }
 }
--- MarcadorWindows/Marcador.cs
+++ MarcadorWindows/Marcador.cs
@@ -7,19 +7,40 @@ using System.Threading.Tasks;
 
 namespace MarcadorWindows
 {
+    public class PartidoEventArgs:EventArgs
+    {
+        public String Ganador { get; set; }
+    }
     internal class Marcador
     {
-        private int[,] marcadorSets;
+        private int[][] marcadorSets;
         private int[] puntos;
         private string[] valorPuntos = { "00", "15", "30", "40", "AV" };
         private int SetActivo;
         private int SetsLocales, SetsVisitantes;
-        private bool PartidoFinalizado;
+        private bool partidoFinalizado;
         private int numSets;
         private const int local = 0;
         private const int visitante = 1;
         private bool tieBreak;
 
+        //Delegado para el evento de finalización del partido
+        
+        //Paso 1: crear delegado
+        public delegate void GestorEventoPartidoFinalizado(object fuente, PartidoEventArgs args);
+        
+        //Paso 2: Crear evento basado en el delegado
+        public event GestorEventoPartidoFinalizado PartidoFinalizado;
+        //public event EventHandler<PartidoEventArgs> PartidoFinalizado;
+
+        //Paso 3: Iniciar el evento
+        protected virtual void OnPartidoFinalizado(string ganador)
+        {
+            if(PartidoFinalizado!=null)
+            {
+                PartidoFinalizado(this, new PartidoEventArgs() { Ganador = ganador });
+            }
+        }
 
         /// <summary>
         /// Constructor por defecto del Marcador
@@ -27,12 +48,14 @@ namespace MarcadorWindows
         public Marcador()
         {
             int i, j;
-            marcadorSets = new int[2, 3];
+            marcadorSets = new int[2][];
+            marcadorSets[local]=new int[3];
+            marcadorSets[visitante] = new int[3];
             for (i = 0; i < 2; i++)
             {
                 for (j = 0; j < 3; j++)
                 {
-                    marcadorSets[i, j] = 0;
+                    marcadorSets[i][j] = 0;
                 }
             }
             puntos = new int[2];
@@ -40,7 +63,7 @@ namespace MarcadorWindows
             JugadorVisitante = "Player 2";
             numSets = 3;
             SetActivo = 0;
-            PartidoFinalizado = false;
+            partidoFinalizado = false;
             SetsLocales = 0;
             SetsVisitantes = 0;
             tieBreak = false;
@@ -56,12 +79,14 @@ namespace MarcadorWindows
         public Marcador(string jugadorLocal, string jugadorVisitante, int numeroSets)
         {
             int i, j;
-            marcadorSets = new int[2, numeroSets];
+            marcadorSets = new int[2][];
+            marcadorSets[local] = new int[3];
+            marcadorSets[visitante] = new int[3];
             for (i = 0; i < 2; i++)
             {
                 for (j = 0; j < 3; j++)
                 {
-                    marcadorSets[i, j] = 0;
+                    marcadorSets[i][j] = 0;
                 }
             }
             puntos = new int[2];
@@ -69,7 +94,7 @@ namespace MarcadorWindows
             JugadorVisitante = jugadorVisitante;
             numSets = numeroSets;
             SetActivo = 0;
-            PartidoFinalizado = false;
+            partidoFinalizado = false;
             SetsLocales = 0;
             SetsVisitantes = 0;
             tieBreak=false;
@@ -84,7 +109,15 @@ namespace MarcadorWindows
         {
             get
             {
-                return valorPuntos[puntos[local]];
+                if (tieBreak)
+                {
+                    return(puntos[local].ToString());
+                }
+                else
+                {
+                    return valorPuntos[puntos[local]];
+                }
+                
             }
         }
 
@@ -92,15 +125,30 @@ namespace MarcadorWindows
         {
             get
             {
-                return valorPuntos[puntos[visitante]];
+                if (tieBreak)
+                {
+                    return puntos[visitante].ToString();
+                }
+                else
+                {
+                    return valorPuntos[puntos[visitante]];
+                }
             }
         }
 
-        public int[,] MarcadorSet
+        public int[] MarcadorLocal
         {
             get
             {
-                return marcadorSets;
+                return marcadorSets[local];
+            }
+        }
+
+        public int[] MarcadorVisitante
+        {
+            get
+            {
+                return marcadorSets[visitante];
             }
         }
 
@@ -110,38 +158,42 @@ namespace MarcadorWindows
         public void PuntoLocal()
         {
             puntos[local]++;
-
-            if (tieBreak)
+            if(!partidoFinalizado)
             {
-                /*Trabajamos por valores, el juego se gana cuando un jugador llega a 7 o más y saca dos de ventaja al otro jugador*/
-                if((puntos[local] >= 7) && ((puntos[local] - puntos[visitante]) >= 2))
+                if (tieBreak)
                 {
-                    puntos[local] = 0;
-                    puntos[visitante] = 0;
-                    JuegoLocal();
-                    tieBreak=false;
+                    /*Trabajamos por valores, el juego se gana cuando un jugador llega a 7 o más y saca dos de ventaja al otro jugador*/
+                    if ((puntos[local] >= 7) && ((puntos[local] - puntos[visitante]) >= 2))
+                    {
+                        puntos[local] = 0;
+                        puntos[visitante] = 0;
+                        JuegoLocal();
+                        tieBreak = false;
+                    }
+                }
+                else
+                {
+                    /* Trabajamos con indices que van de 0 a 4, en el vector valorPuntos tenemos las conversiones de esos puntos a formato tenis o padel.
+                     * de esta forma el indice 0 se corresponde con el valor 00, el 1 con 15, 2 con 30 y asi sucesivamente.
+                     * Comprobamos que cuando lleguemos a 4 sacamos 2 de ventaja al otro jugador para conseguir el juego, si no sacamos 2 de ventaja estaremos en una situacion de ventaja
+                     * por lo que si el otro jugador estaba en ventaja y conseguimos punto estaremos en iguales, si estamos en iguales nos pondremos con ventaja
+                     * y si estabamos en ventaja, nos separaremos en 2 por lo que será nuestro el juego*/
+                    if ((puntos[local] >= 4) && ((puntos[local] - puntos[visitante]) >= 2))
+                    {
+                        puntos[local] = 0;
+                        puntos[visitante] = 0;
+                        JuegoLocal();
+                    }
+                    else if ((puntos[local] >= 4) && (puntos[visitante] >= 4))
+                    {
+                        puntos[visitante]--;
+                        puntos[local]--;
+                    }
+                    //Debug.WriteLine("local: {0}", valorPuntos[puntos[local]]);
+                    //Debug.WriteLine("visitante: {0}", valorPuntos[puntos[visitante]]);
                 }
             }
-            else {
-                /* Trabajamos con indices que van de 0 a 4, en el vector valorPuntos tenemos las conversiones de esos puntos a formato tenis o padel.
-                 * de esta forma el indice 0 se corresponde con el valor 00, el 1 con 15, 2 con 30 y asi sucesivamente.
-                 * Comprobamos que cuando lleguemos a 4 sacamos 2 de ventaja al otro jugador para conseguir el juego, si no sacamos 2 de ventaja estaremos en una situacion de ventaja
-                 * por lo que si el otro jugador estaba en ventaja y conseguimos punto estaremos en iguales, si estamos en iguales nos pondremos con ventaja
-                 * y si estabamos en ventaja, nos separaremos en 2 por lo que será nuestro el juego*/
-                if ((puntos[local] >= 4) && ((puntos[local] - puntos[visitante]) >= 2))
-                {
-                    puntos[local] = 0;
-                    puntos[visitante] = 0;
-                    JuegoLocal();
-                }
-                else if ((puntos[local] >= 4) && (puntos[visitante] >= 4))
-                {
-                    puntos[visitante]--;
-                    puntos[local]--;
-                }
-                //Debug.WriteLine("local: {0}", valorPuntos[puntos[local]]);
-                //Debug.WriteLine("visitante: {0}", valorPuntos[puntos[visitante]]);
-            }
+            
 
 
         }
@@ -151,38 +203,43 @@ namespace MarcadorWindows
         public void PuntoVisitante()
         {
             puntos[visitante]++;
-            if (tieBreak)
+            if(!partidoFinalizado)
             {
-                /*Trabajamos por valores, el juego se gana cuando un jugador llega a 7 o más y saca dos de ventaja al otro jugador*/
-                if ((puntos[visitante] >= 7) && ((puntos[visitante] - puntos[local]) >= 2))
+                if (tieBreak)
                 {
-                    puntos[local] = 0;
-                    puntos[visitante] = 0;
-                    JuegoVisitante();
-                    tieBreak = false;
+                    /*Trabajamos por valores, el juego se gana cuando un jugador llega a 7 o más y saca dos de ventaja al otro jugador*/
+                    if ((puntos[visitante] >= 7) && ((puntos[visitante] - puntos[local]) >= 2))
+                    {
+                        puntos[local] = 0;
+                        puntos[visitante] = 0;
+                        JuegoVisitante();
+                        tieBreak = false;
+                    }
+                }
+                else
+                {
+                    /* Trabajamos con indices que van de 0 a 4, en el vector valorPuntos tenemos las conversiones de esos puntos a formato tenis o padel.
+                     * de esta forma el indice 0 se corresponde con el valor 00, el 1 con 15, 2 con 30 y asi sucesivamente.
+                     * Comprobamos que cuando lleguemos a 4 sacamos 2 de ventaja al otro jugador para conseguir el juego, si no sacamos 2 de ventaja estaremos en una situacion de ventaja
+                     * por lo que si el otro jugador estaba en ventaja y conseguimos punto estaremos en iguales, si estamos en iguales nos pondremos con ventaja
+                     * y si estabamos en ventaja, nos separaremos en 2 por lo que será nuestro el juego*/
+                    if ((puntos[visitante] >= 4) && ((puntos[visitante] - puntos[local]) >= 2))
+                    {
+                        puntos[local] = 0;
+                        puntos[visitante] = 0;
+                        JuegoVisitante();
+
+                    }
+                    else if ((puntos[visitante] >= 4) && (puntos[local] >= 4))
+                    {
+                        puntos[visitante]--;
+                        puntos[local]--;
+                    }
+                    //Debug.WriteLine("local", valorPuntos[puntos[local]]);
+                    //Debug.WriteLine("visitante", valorPuntos[puntos[visitante]]);
                 }
             }
-            else {
-                /* Trabajamos con indices que van de 0 a 4, en el vector valorPuntos tenemos las conversiones de esos puntos a formato tenis o padel.
-                 * de esta forma el indice 0 se corresponde con el valor 00, el 1 con 15, 2 con 30 y asi sucesivamente.
-                 * Comprobamos que cuando lleguemos a 4 sacamos 2 de ventaja al otro jugador para conseguir el juego, si no sacamos 2 de ventaja estaremos en una situacion de ventaja
-                 * por lo que si el otro jugador estaba en ventaja y conseguimos punto estaremos en iguales, si estamos en iguales nos pondremos con ventaja
-                 * y si estabamos en ventaja, nos separaremos en 2 por lo que será nuestro el juego*/
-                if ((puntos[visitante] >= 4) && ((puntos[visitante] - puntos[local]) >= 2))
-                {
-                    puntos[local] = 0;
-                    puntos[visitante] = 0;
-                    JuegoVisitante();
-
-                }
-                else if ((puntos[visitante] >= 4) && (puntos[local] >= 4))
-                {
-                    puntos[visitante]--;
-                    puntos[local]--;
-                }
-                //Debug.WriteLine("local", valorPuntos[puntos[local]]);
-                //Debug.WriteLine("visitante", valorPuntos[puntos[visitante]]);
-                }
+            
 
         }
 
@@ -191,29 +248,58 @@ namespace MarcadorWindows
         /// </summary>
         private void JuegoLocal()
         {
-            marcadorSets[local, SetActivo]++;
-            if ((marcadorSets[local, SetActivo] >= 6) && ((marcadorSets[local, SetActivo] - marcadorSets[visitante, SetActivo]) >= 2))
+            marcadorSets[local][SetActivo]++;
+            if (tieBreak)
             {
                 SetLocal();
-
             }
+            else
+            {
+                if ((marcadorSets[local][SetActivo] >= 6) && ((marcadorSets[local][SetActivo] - marcadorSets[visitante][SetActivo]) >= 2))
+                {
+                    SetLocal();
+
+                }
+            }
+            
             //Debug.WriteLine("Set local {0}", marcadorSets[local, SetActivo]);
             //Debug.WriteLine("Set visitante {0}", marcadorSets[visitante, SetActivo]);
-
+            
+            //Si en el set vamos 6-6 pasamos a situacion de tieBreak
+            if ((marcadorSets[local][SetActivo] == 6)&& (marcadorSets[visitante][SetActivo] == 6))
+            {
+                tieBreak = true;
+                puntos[local] = 0;
+                puntos[visitante] = 0;
+            }
         }
         /// <summary>
         /// Añade un juego al jugador visitante
         /// </summary>
         private void JuegoVisitante()
         {
-            marcadorSets[visitante, SetActivo]++;
-            if ((marcadorSets[visitante, SetActivo] >= 6) && ((marcadorSets[visitante, SetActivo] - marcadorSets[local, SetActivo]) >= 2))
+            marcadorSets[visitante][SetActivo]++;
+            if (tieBreak)
             {
                 SetVisitante();
+            }
+            else
+            {
+                if ((marcadorSets[visitante][SetActivo] >= 6) && ((marcadorSets[visitante][SetActivo] - marcadorSets[local][SetActivo]) >= 2))
+                {
+                    SetVisitante();
+                }
             }
             //Debug.WriteLine("Set local  {0}", marcadorSets[local, SetActivo]);
             //Debug.WriteLine("Set visitante  {0}", marcadorSets[visitante, SetActivo]);
 
+            //Si en el set vamos 6-6 pasamos a situacion de tieBreak
+            if ((marcadorSets[local][SetActivo] == 6) && (marcadorSets[visitante][SetActivo] == 6))
+            {
+                tieBreak = true;
+                puntos[local] = 0;
+                puntos[visitante] = 0;
+            }
         }
         /// <summary>
         /// Añade un set al jugador Local
@@ -223,7 +309,8 @@ namespace MarcadorWindows
             SetsLocales++;
             if (SetsLocales > (numSets / 2))
             {
-                PartidoFinalizado = true;
+                partidoFinalizado = true;
+                OnPartidoFinalizado(JugadorLocal);
             }
             else
             {
@@ -238,7 +325,8 @@ namespace MarcadorWindows
             SetsVisitantes++;
             if (SetsVisitantes > (numSets / 2))
             {
-                PartidoFinalizado = true;
+                partidoFinalizado = true;
+                OnPartidoFinalizado(JugadorVisitante);
             }
             else
             {
